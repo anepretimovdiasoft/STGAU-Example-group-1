9a9cf9bca74df5efc76de5873454b80c696f3c8a
Solnce009 <>
2025-03-01 15:32:45 +0300

    add Student

--- src/main/java/ru/example/edu/controller/HelloController.java
+++ src/main/java/ru/example/edu/controller/HelloController.java
@@ -3,13 +3,25 @@ package ru.example.edu.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.example.edu.model.Student;
 
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(@RequestParam String name) {
-        return "Hello" + name;
+        return "Hello " + name;
+    }
+
+    @GetMapping("/student")
+    public Student getStudent() {
+        Student st = new Student(
+                1,
+                "Valer",
+                "valera@ya.ru",
+                19
+        );
+        return st;
     }
 
 }
